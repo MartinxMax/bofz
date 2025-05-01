fce57ac108d5d21c49e209dc8e461b179ec6108d
Мартин <85664564+MartinxMax@users.noreply.github.com>
2025-05-01 19:20:36 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# BOFZ
+
+BOFZ is a simple Buffer Overflow Scanner designed to detect buffer overflow vulnerabilities in a given executable.This tool is useful for quickly testing applications or binaries for common security flaws, particularly those that arise from improper bounds checking when handling user input.
+
+# Usage
+
+`$ ./bofz.sh <bin_file>`
+
+`$ ./bofz.sh ./example`
+
+
+![alt text](./pic/image.png)
+
+ 
---
+++ bofz.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+FILENAME=$1
+MAX=16384
+
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[1;34m'
+CYAN='\033[1;36m'
+RESET='\033[0m'
+
+clear
+echo -e "${CYAN}__________ ________  _____________________${RESET}"
+echo -e "${CYAN}\\______   \\\\_____  \\ \\_   _____/\\____    /${RESET}"
+echo -e "${CYAN} |    |  _/ /   |   \\ |    __)    /     / ${RESET}"
+echo -e "${CYAN} |    |   \\/    |    \\|     \\    /     /_ ${RESET}"
+echo -e "${CYAN} |______  /\\_______  /\\___  /   /_______ \\ ${RESET}"
+echo -e "${CYAN}        \\/         \\/     \\/            \\/ ${RESET}"
+echo -e "${YELLOW}             BOFZ Buffer Overflow Scanner${RESET}"
+echo -e "${GREEN}Maptnh@S-H4CK13 https://github.com/MartinxMax ${RESET}"
+echo -e '--------------------------------------------------------'
+
+if [ $# -ne 1 ]; then
+  echo -e "${RED}[×] Usage: $0 <executable path>${RESET}"
+  exit 1
+fi
+
+for size in $(seq 1 $MAX); do
+  printf "\r${YELLOW}[*] Trying buffer size: %4d ${RESET}" "$size"
+  input=$(head -c "$size" </dev/zero | tr '\0' 'A')
+  setsid bash -c "(echo -n \"$input\"; printf '\n') | \"$FILENAME\"" > /dev/null 2>&1
+  status=$?
+  if [ $status -eq 139 ]; then
+    printf "\n${RED}[!] Buffer overflow detected at: %d bytes${RESET}\n" "$size"
+    exit 0
+  fi
+done
+
+printf "\n${GREEN}[✓] No buffer overflow detected in 1–%d bytes range${RESET}\n" "$MAX"
+exit 0
